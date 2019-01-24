ed4574dad6d3096d9bf8656c6e9c601fcc3e6689
Igor Golovanov <igor.golovanov@gmail.com>
2019-01-24 12:54:45 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# hapi-transaction
+
+[![Build Status](https://travis-ci.org/igorgolovanov/hapi-transaction.svg?branch=master&style=flat)](https://travis-ci.org/igorgolovanov/hapi-transaction)
+![Current Version](https://img.shields.io/npm/v/hapi-transaction.svg?style=flat)
+
---
+++ test/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Code = require('code');
+const Hapi = require('hapi');
+const Lab = require('lab');
+const Hal = require('..');
+
+
+const internals = {};
+
+
+const { describe, it } = exports.lab = Lab.script();
+const expect = Code.expect;
+
+describe('register()', () => {
+
+});
